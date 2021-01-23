09b432f0c79ec0686050d7b79657da20c4863f0f
VFelix <ge38xes@mytum.de>
2021-01-23 13:07:52 +0100

    added flag rotation script

--- SailAway/Assets/Scripts/InputSideEffects/WindRotation.cs
+++ SailAway/Assets/Scripts/InputSideEffects/WindRotation.cs
@@ -13,24 +13,28 @@ public class WindRotation : MonoBehaviour
     [SerializeField]
     private GameObject shipObject;
     private IShipSignals _shipSignals;
+    private GameObject _flag;
 
     Vector3 dir;
 
     private void Awake()
     {
         _shipSignals = shipObject.GetComponent<ShipController>();
+        _flag = gameObject;
     }
 
     private void Start()
     {
-        _shipSignals.windDirection.Subscribe(v3 =>
+        _shipSignals.windDirection.Subscribe(a =>
         {
-            dir = -v3;
+            _flag.transform.localRotation = Quaternion.Euler(0f, Vector3.SignedAngle(shipObject.transform.forward, a, new Vector3(0, 1, 0)), 0f);
+        }).AddTo(this);
+        _shipSignals.shiplTiltRelative.Subscribe(_ =>
+        {
+            _flag.transform.localRotation =
+                Quaternion.Euler(0f, Vector3.SignedAngle(shipObject.transform.forward, _shipSignals.windDirection.Value, new Vector3(0, 1, 0)), 0f);
         }).AddTo(this);
     }
 
-    void Update()
-    {
-        transform.LookAt(transform.position + dir, Vector3.up);
-    }
+    
 }
