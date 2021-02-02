305749ed1ae67b0692615a595e1925aa6b712ecb
VFelix <ge38xes@mytum.de>
2021-02-02 10:52:01 +0100

    turbo birds are now better visible

--- SailAway/Assets/Scripts/CollectibleSideEffects/BirdPositioner.cs
+++ SailAway/Assets/Scripts/CollectibleSideEffects/BirdPositioner.cs
@@ -7,7 +7,7 @@ public class BirdPositioner : MonoBehaviour
 {
     public GameObject[] birds;
     public float minRadius = 0.5f;
-    public float maxRadius = 4f;
+    public float maxRadius = 8f;
     private float maxNoise;
     private float _radius;
     private float _targetRadius;
@@ -24,9 +24,9 @@ public class BirdPositioner : MonoBehaviour
     {
         Vector3 target = new Vector3(playerShip.transform.position.x, 0, playerShip.transform.position.z);
         Vector3 central = new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z);
-        _targetRadius = Vector3.Distance(target, central)/10f;
+        _targetRadius = Vector3.Distance(target, central)/10f -10f;
         _radius = Mathf.Lerp(_radius, Mathf.Clamp(_targetRadius, minRadius, maxRadius), 0.5f);
-        var size = ((_radius - minRadius) / (maxRadius - minRadius)) * 8 + 2;
+        var size = ((_radius - minRadius) / (maxRadius - minRadius)) * 5 + 2;
         for (int i = 0; i < birds.Length; i++)
         {
             float angleSpeed = speed / _radius;
